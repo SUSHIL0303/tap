f3de53ccd17f76ebd09ba9a84346b56ace0c8040
SUSHIL0303 <sushilabish15670@gmail.com>
2026-03-05 21:00:04 +0530

    404 file deleted and main.jsx file updated with hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import App from "./App"
 import "./styles/global.css"
 
-const params = new URLSearchParams(window.location.search)
-const redirect = params.get("p")
-
-if (redirect) {
-  window.history.replaceState(null, "", "/tap" + redirect)
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tap">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
