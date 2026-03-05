a5ce32c35bc84c7f3c64bd968e8074e150456feb
SUSHIL0303 <sushilabish15670@gmail.com>
2026-03-05 20:46:27 +0530

    404 html file updated and main.jsx file updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./styles/global.css"
 
+const params = new URLSearchParams(window.location.search)
+const redirect = params.get("p")
+
+if (redirect) {
+  window.history.replaceState(null, "", redirect)
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/tap">
